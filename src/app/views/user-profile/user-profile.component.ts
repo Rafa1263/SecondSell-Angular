80652dca9d46a50d178d6759858db03cd4ba530d
Rafael Perez <rafael.perez@w2m.com>
2023-05-18 18:49:35 +0200

    update on user view

--- src/app/views/user-profile/user-profile.component.ts
+++ src/app/views/user-profile/user-profile.component.ts
@@ -19,7 +19,7 @@ import { ProductService } from 'src/app/services/products.service';
 export class UserProfileComponent implements OnInit {
   public product: Product = {} as Product
   public user: User = {} as User
-  public selection = 2
+  public selection = 1
   constructor(private chatService: ChatService, private router: Router, private route: ActivatedRoute, private productService: ProductService, private authService: AuthService, private categoryService: CategoryService) {
 
   }
